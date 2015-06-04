d7cd2b3b975abec2f1d0b744ca8c02afe324b38c
mclucario <mclucario@freenet.de>
2015-06-04 22:20:53 +0200

    Main Add

---
+++ Minecraft Mod Manager/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(290, 25)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(269, 251)
+        Me.ListBox1.Sorted = True
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(15, 25)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.ScrollAlwaysVisible = True
+        Me.ListBox2.Size = New System.Drawing.Size(269, 251)
+        Me.ListBox2.Sorted = True
+        Me.ListBox2.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(544, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Toggle"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Disabled:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(287, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enabled:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(562, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Minecraft Version:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Items.AddRange(New Object() {"1.8.3", "1.8.2", "1.8.1", "1.8", "1.7.10", "1.7.9", "1.7.5", "1.7.4", "1.7.2", "1.6.4", "1.6.2", "1.6.1", "1.5.2", "1.5.1", "1.4.7", "1.4.6", "1.4.5", "1.4.4", "1.4.2", "1.3.2", "1.3.1", "1.2.5", "1.2.4", "1.2.3", "1.2.2", "1.2.1", "1.1", "1.0"})
+        Me.ComboBox1.Location = New System.Drawing.Point(562, 41)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(118, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(565, 282)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(115, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Import Mod"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "MinecraftMod.jar"
+        Me.OpenFileDialog1.Filter = "Minecraft Forge Mods|*.jar|Liteloader Mods|*.liteloader"
+        Me.OpenFileDialog1.Title = "Import MOD"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(562, 68)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(101, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Liteloader Mods"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(565, 253)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(115, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(565, 132)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(115, 115)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(565, 116)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(57, 13)
+        Me.LinkLabel1.TabIndex = 11
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LiteLoader"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(565, 103)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(81, 13)
+        Me.LinkLabel2.TabIndex = 12
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Minecraft Forge"
+        '
+        'Form1
+        '
+        Me.AccessibleName = "Minecraft Mod Manager 1.0"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(692, 321)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Simple Minecraft Mod Manager"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Minecraft Mod Manager/Form1.vb
@@ -0,0 +1,200 @@
+﻿Imports System
+Imports System.IO
+
+Public Class Form1
+
+    Public ModFolder As String
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        If e.KeyCode = Keys.F5 Then
+
+            ReloadMods()
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Load the modlist for the standart version.
+        ComboBox1.SelectedItem = "1.8"
+
+        ReloadMods()
+
+    End Sub
+
+    Function ReloadMods()
+
+        'Create Mod Folder Variable.
+
+        If CheckBox1.Checked Then
+            ModFolder = Environ$("appdata") + "/.minecraft/mods"
+        Else
+            ModFolder = Environ$("appdata") + "/.minecraft/mods/" + ComboBox1.Text
+        End If
+
+        'Clear List
+
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+
+        'Check if the folder for mods exists and check that it doesn't generates a no version folder.
+
+        If Not My.Computer.FileSystem.DirectoryExists(ModFolder) Then
+            My.Computer.FileSystem.CreateDirectory(ModFolder)
+        End If
+
+        'Check if the folder for disabled mods exists.
+
+        If Not My.Computer.FileSystem.DirectoryExists(ModFolder + "/_disabled/") Then
+            My.Computer.FileSystem.CreateDirectory(ModFolder + "/_disabled/")
+        End If
+
+        'Enabled Mods
+
+        For Each ModFile In Directory.GetFiles(ModFolder)
+            ListBox1.Items.Add(ModFile.Replace(ModFolder + "\", ""))
+        Next
+
+        'Disabled Mods
+
+        For Each ModFile In Directory.GetFiles(ModFolder + "/_disabled/")
+            ListBox2.Items.Add(ModFile.Replace(ModFolder + "/_disabled/", ""))
+        Next
+
+        Return Nothing
+
+    End Function
+
+    Private Sub ComboBox1_Menu(sender As Object, e As EventArgs) Handles ComboBox1.SelectionChangeCommitted
+
+        'Change mod folder on version change
+
+        ReloadMods()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Toggle a mod.
+
+        If ListBox1.SelectedItems.Count = 0 And ListBox2.SelectedItems.Count = 0 Then
+
+            MessageBox.Show("Please select a mod to first.")
+
+        ElseIf ListBox1.SelectedItems.Count = 1 And ListBox2.SelectedItems.Count = 0 Then
+
+            If File.Exists(ModFolder + "/" + ListBox1.SelectedItem) Then
+                My.Computer.FileSystem.MoveFile(ModFolder + "/" + ListBox1.SelectedItem, ModFolder + "/_disabled/" + ListBox1.SelectedItem)
+            End If
+
+        ElseIf ListBox1.SelectedItems.Count = 0 And ListBox2.SelectedItems.Count = 1 Then
+
+            If File.Exists(ModFolder + "/_disabled/" + ListBox2.SelectedItem) Then
+                My.Computer.FileSystem.MoveFile(ModFolder + "/_disabled/" + ListBox2.SelectedItem, ModFolder + "/" + ListBox2.SelectedItem)
+            End If
+
+        End If
+
+        ReloadMods()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        'Import a new mod
+
+        If CheckBox1.Checked Then
+
+            OpenFileDialog1.Filter = "Liteloader Mods|*.litemod"
+            OpenFileDialog1.FileName = "LiteLoaderMod.litemod"
+
+        Else
+
+            OpenFileDialog1.Filter = "Minecraft Forge Jar Mods|*.jar"
+            OpenFileDialog1.FileName = "MinecraftForgeMod.jar"
+
+        End If
+
+        OpenFileDialog1.ShowDialog()
+
+        If File.Exists(OpenFileDialog1.FileName) Then
+            My.Computer.FileSystem.MoveFile(OpenFileDialog1.FileName, ModFolder + "/_disabled/" + Path.GetFileName(OpenFileDialog1.FileName))
+        End If
+
+        OpenFileDialog1.Dispose()
+
+        ReloadMods()
+
+    End Sub
+
+    Private Sub CheckBox1_Click(sender As Object, e As EventArgs) Handles CheckBox1.Click
+
+        If CheckBox1.Checked Then
+            ComboBox1.Enabled = False
+            ComboBox1.Items.Add("No Version Control")
+            ComboBox1.Text = "No Version Control"
+        Else
+            ComboBox1.Text = "1.8"
+            ComboBox1.Items.Remove("No Version Control")
+            ComboBox1.Enabled = True
+        End If
+
+        ReloadMods()
+
+    End Sub
+
+    Private Sub ListBox1_Click(sender As Object, e As EventArgs) Handles ListBox1.Click
+
+        ListBox2.ClearSelected()
+
+    End Sub
+
+    Private Sub ListBox2_Click(sender As Object, e As EventArgs) Handles ListBox2.Click
+
+        ListBox1.ClearSelected()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        If ListBox1.SelectedItems.Count = 1 And ListBox2.SelectedItems.Count = 0 Then
+
+            If File.Exists(ModFolder + "/" + ListBox1.SelectedItem) Then
+                My.Computer.FileSystem.DeleteFile(ModFolder + "/" + ListBox1.SelectedItem)
+            End If
+
+        ElseIf ListBox1.SelectedItems.Count = 0 And ListBox2.SelectedItems.Count = 1 Then
+
+            If File.Exists(ModFolder + "/_disabled/" + ListBox2.SelectedItem) Then
+                My.Computer.FileSystem.DeleteFile(ModFolder + "/_disabled/" + ListBox2.SelectedItem)
+            End If
+
+        End If
+
+        ReloadMods()
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        Process.Start("http://mclucario.de/wordpress/programs")
+
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+
+        Process.Start("http://files.minecraftforge.net/#Downloads")
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+
+        Process.Start("http://www.liteloader.com/")
+
+    End Sub
+
+
+
+End Class
